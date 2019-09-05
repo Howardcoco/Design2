a5e13c98cb1c1635a882b5d27adc3c582985fe1e
Howard <howard.hu@eeewa.org>
2019-09-05 21:19:18 +0800

    view change

--- webdesign/firstapp/views.py
+++ webdesign/firstapp/views.py
@@ -3,39 +3,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import boto3
 
-# dynamodb = boto3.resource('dynamodb')
-# table = dynamodb.Table('designDB2')
-#
-# response = table.get_item(
-#    Key={
-#         'time': '2019-9-4 8:28:35 PM',
-#         'sensor_message': 'water'
-#     }
-# )
-# item = response['Item']
-    dynamo_response = get_dynamo_data()
-    items = dynamo_response['Items']
-    data = parse_data(items)
-    print(item)
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('designDB2')
 
+response = table.get_item(
+   Key={
+        'time': '2019-9-4 8:28:35 PM',
+        'sensor_message': 'water'
+    }
+)
+item = response['Item']
+print(item)
 def index(request):
     context=item
     print(item)
     return HttpResponse(format(context))
-
-def get_dynamo_data():
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('designDB2')
-    pe = 'time, sensor_message'
-    response = table.scan(
-        ProjectionExpression=pe,
-        )
-    while 'sensor_message' in response:
-        response = table.scan(
-            ProjectionExpression=pe,
-            ExclusiveStartKey=response['sensor_message']
-            )
-    return response
 
 # def search(request):
 #     # template = loader.get_template('firstapp/search.html')
